063aa8528752c6b8f78615a92b7c8363b88eb9a8
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 07:14:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,8 +53,8 @@ class Song
     artist = array[0].strip
     song = array[1].strip
     new_song = self.find_or_create_by_name
-    new_song.song = 
-    new.artist_name = artist 
+    new_song.name = song
+    new_song.artist_name = artist 
   end
   
   
