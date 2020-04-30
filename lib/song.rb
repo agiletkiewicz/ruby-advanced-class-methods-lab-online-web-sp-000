6d397e6c17b1f4fda5f7ccacad256d841751680d
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 06:11:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,31 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song 
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new 
+    song.name = song_name 
+    song
+  end
+  
+   def self.create_by_name(song_name)
+     song = self.new 
+     @@all << song
+     song.name = song_name 
+     song
+  end
+  
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+  
+  
 
 end
